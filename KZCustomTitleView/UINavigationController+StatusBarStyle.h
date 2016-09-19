48d8a1895cda541520bf8176df30dc4c264fda65
GhostKZShadow <GhostKZShadow@163.com>
2016-09-19 15:11:22 +0800

    设置状态栏的字体为白色

---
+++ KZCustomTitleView/UINavigationController+StatusBarStyle.h
@@ -0,0 +1,25 @@
+//
+//  UINavigationController+StatusBarStyle.h
+//  MyNavigationBar
+//
+//  Created by 龙邦 on 16/9/19.
+//  Copyright © 2016年 longwatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationController (StatusBarStyle)
+
+@end
+
+
+/*
+
+
+
+            使状态栏的字体变为白色
+
+
+
+
+*/
---
+++ KZCustomTitleView/UINavigationController+StatusBarStyle.m
@@ -0,0 +1,15 @@
+//
+//  UINavigationController+StatusBarStyle.m
+//  MyNavigationBar
+//
+//  Created by 龙邦 on 16/9/19.
+//  Copyright © 2016年 longwatch. All rights reserved.
+//
+
+#import "UINavigationController+StatusBarStyle.h"
+
+@implementation UINavigationController (StatusBarStyle)
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
+}
+@end
